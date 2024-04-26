dc31d86299dfbc298476645a27783fa7091556a0
Kali Rishi K <110583211+kalirishik@users.noreply.github.com>
2024-04-26 12:06:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int a[]=new int[s+1];
+        int i,j,m=0;
+        for( i=0;i<s;i++){
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<s;i++){
+            for(j=0;j<s;j++){
+                if(a[i]>a[j+1]){
+
+                }
+
+
+            }
+
+        }
+    }
+}
